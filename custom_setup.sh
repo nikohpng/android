c7c2f46332f0920fee97af01312f124c0c07aa3e
hpng <1455202511@qq.com>
2023-06-30 14:28:37 +0800

    加密

---
+++ custom_setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+VERSION=104.5112.03.yw.6
+
+RELEASE_VERSION=${VERSION}
+
+SDK_BIN_URL=https://github.com/nikohpng/android/releases/download/${VERSION}/libwebrtc.aar
+
+BUILD_DIR=build
+mkdir -p ${BUILD_DIR}
+cd ${BUILD_DIR}
+
+echo "Downloading webrtc-sdk ${VERSION} binary for android."
+curl -L -O ${SDK_BIN_URL}
+
+mvn install:install-file \
+    -Dfile=libwebrtc.aar \
+    -Dpackaging=aar \
+    -Dversion=${RELEASE_VERSION} \
+    -DgroupId=com.github.nikohpng \
+    -DartifactId=android
